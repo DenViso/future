485d11cd1ad04ec18413bdedc229a3f37604bd86
DenViso <itdeniro00@gmail.com>
2024-04-04 09:15:56 +0300

    chenge stoun

--- src/components/Section/Ring.jsx
+++ src/components/Section/Ring.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 export const Ring = () => {
   return (
-     <div className='ring'>
-     <Link to="/future/">На головну</Link>
-     <h1>Тут будуть прикраси</h1>
-     </div>
-  )
-}
+    <div className="ring">
+      <Link to="/future/">На головну</Link>
+      <h1>Прикраси</h1>
+      <ul className="ringList">
+        <li>
+          <Link to="/future/Ring/1">
+            Заручини
+            <img src="./img/subsect/1.png" alt="" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/future/Ring/1">
+          Обручки
+            <img src="./img/subsect/2.png" alt="" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/future/Ring/1">
+          Жіночі
+            <img src="./img/subsect/3.png" alt="" />
+          </Link>
+          </li>
+          <li>
+                      <Link to="/future/Ring/1">
+          Чоловічі
+            <img src="./img/subsect/4.png" alt="" />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
