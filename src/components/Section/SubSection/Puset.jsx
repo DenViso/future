3d1cb5d94b1905c6708458925621f0094115808c
den viso <itdeniro00@gmail.com>
2024-12-02 18:34:27 +0200

    dellet layziload in Ring

--- src/components/Section/SubSection/Puset.jsx
+++ src/components/Section/SubSection/Puset.jsx
@@ -5,21 +5,37 @@ import LazyLoad from "react-lazyload";
 export const Puset = ({ t }) => {
   return (
     <div className="ring">
-      <Link className="back" to="/Ring">{t("back.toJewelry")}</Link>
+      <Link className="back" to="/Ring">
+        {t("back.toJewelry")}
+      </Link>
       <h2>{t("section.section12")}</h2>
       <ul className="ringList">
         <li>
           {/* соло */}
           <Link to="Inner/35">
             {t("section.section5")}
-            <LazyLoad><img className="ringImg" loading="lazy" src="/img/sect/jew/pus/1.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+              <img
+                className="ringImg"
+                loading="lazy"
+                src="/img/sect/jew/pus/1.jpg"
+                alt=""
+              />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           {/* соло+розсип */}
           <Link to="inner/33">
             {t("section.section6")}
-            <LazyLoad><img className="ringImg" loading="lazy" src="/img/sect/jew/pus/2.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+              <img
+                className="ringImg"
+                loading="lazy"
+                src="/img/sect/jew/pus/2.jpg"
+                alt=""
+              />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
@@ -27,7 +43,14 @@ export const Puset = ({ t }) => {
           <Link to="inner/32">
             {" "}
             {t("section.section7")}
-            <LazyLoad><img className="ringImg" loading="lazy" src="/img/sect/jew/pus/3.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+              <img
+                className="ringImg"
+                loading="lazy"
+                src="/img/sect/jew/pus/3.jpg"
+                alt=""
+              />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
@@ -35,7 +58,14 @@ export const Puset = ({ t }) => {
           <Link to="inner/34">
             {" "}
             {t("section.section8")}
-            <LazyLoad><img className="ringImg" loading="lazy" src="/img/sect/jew/pus/4.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+              <img
+                className="ringImg"
+                loading="lazy"
+                src="/img/sect/jew/pus/4.jpg"
+                alt=""
+              />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
@@ -43,7 +73,14 @@ export const Puset = ({ t }) => {
           <Link to="inner/37">
             {" "}
             {t("section.section9")}
-            <LazyLoad><img className="ringImg" loading="lazy" src="/img/sect/jew/pus/5.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+              <img
+                className="ringImg"
+                loading="lazy"
+                src="/img/sect/jew/pus/5.jpg"
+                alt=""
+              />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
@@ -51,7 +88,14 @@ export const Puset = ({ t }) => {
           <Link to="inner/36">
             {" "}
             {t("section.section22")}
-            <LazyLoad><img className="ringImg" loading="lazy" src="/img/sect/jew/pus/6.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+              <img
+                className="ringImg"
+                loading="lazy"
+                src="/img/sect/jew/pus/6.jpg"
+                alt=""
+              />
+            {/* </LazyLoad> */}
           </Link>
         </li>
       </ul>
