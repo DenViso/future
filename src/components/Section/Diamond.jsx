b27d48618467e2637f083d05c3d018d8a089bf2e
den viso <itdeniro00@gmail.com>
2024-12-02 18:28:35 +0200

    dellet layziload in Ring

--- src/components/Section/Diamond.jsx
+++ src/components/Section/Diamond.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LazyLoad from "react-lazyload";
+// import LazyLoad from "react-lazyload";
 export const Diamond = ({ t }) => {
   return (
     <div className="diamond">
@@ -12,71 +12,81 @@ export const Diamond = ({ t }) => {
         <li>
           <Link to="/">
             <h2>{t("care.stone4")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/1.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/1.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title1")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/2.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/2.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("care.stone3")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/3.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/3.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title2")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/4.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/4.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title3")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/5.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/5.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title4")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/6.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/6.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title5")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/1.jpeg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/1.jpeg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title6")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/7.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/7.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("care.stone5")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/8.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/8.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
         <li>
           <Link>
             <h2>{t("stones.title7")}</h2>
-            <LazyLoad>
-            <img className="diamondImg" loading="lazy" src="./img/stoun/9.jpg" alt="" /></LazyLoad>
+            {/* <LazyLoad> */}
+            <img className="diamondImg" loading="lazy" src="./img/stoun/9.jpg" alt="" />
+            {/* </LazyLoad> */}
           </Link>
         </li>
       </ul>
