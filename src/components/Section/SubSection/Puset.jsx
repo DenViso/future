ec5856823ed41a22ccd5d430e63804bd75380fbf
den viso <itdeniro00@gmail.com>
2024-12-02 18:35:24 +0200

    dellet layziload in Ring

--- src/components/Section/SubSection/Puset.jsx
+++ src/components/Section/SubSection/Puset.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LazyLoad from "react-lazyload";
+// import LazyLoad from "react-lazyload";
 
 export const Puset = ({ t }) => {
   return (
